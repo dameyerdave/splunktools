c63beec6e047a6093cb07a145d61c0817d229ae0
David Meyer <david.meyer@terreactive.ch>
2018-04-04 11:15:50 +0200

    first running version

---
+++ config.sh
@@ -0,0 +1,11 @@
+# Splunk home directory
+SPLHOME=/opt/splunk
+
+# Path to splunk apps
+SPLAPPS="${SPLHOME}/etc/apps"
+
+# Path to splunk exe
+SPL="${SPLHOME}/bin/splunk"
+
+# Shortcut for btool
+BT="${SPL} btool"
