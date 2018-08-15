dccb9b5cdfbc9a5a70d1497054d98de8eb36f4a6
David Meyer <david.meyer@terreactive.ch>
2018-08-15 12:16:57 +0200

    added stcert utility

--- config.sh
+++ config.sh
@@ -10,5 +10,14 @@ SPL="${SPLHOME}/bin/splunk"
 # Shortcut for btool
 BT="${SPL} btool"
 
+# Certificates directory
+CERTDIR="${SPLHOME}/etc/certs"
+
+# Certificate Subject
+CERTSUBJ="/C=CH/ST=CH/O=Company/CN=$(hostname -f)"
+
+# Certificate alternative names
+CERTALT="DNS:$(hostname),DNS:$(hostname -f)"
+
 # Management Port
 MGMTPORT=8089
